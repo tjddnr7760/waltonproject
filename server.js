edebcf575b74f6c20edef5dcc44257c67d2a33b6
yellocow <tjddnr7760@naver.com>
2019-01-31 14:19:32 +0900

    fs

--- server.js
+++ server.js
@@ -1,24 +1,5 @@
-var Iamport = require('iamport');
-var iamport = new Iamport({
-  impKey: '3896330295252123',
-  impSecret: 'dOatHlw3dsIxPLLKi0iv7xS1KKjg4gWh47LMshPGKydAkYkCZC76a08VKrbPH87MfSyHaooUXBuhAPTA'
-});
-
-// 아임포트 고유 아이디로 결제 정보를 조회
-iamport.payment.getByImpUid({
-  imp_uid: 'imp92825396'  
-}).then(function(result){
-  // To do
-}).catch(function(error){
-  // handle error
-});
-
-// 상점 고유 아이디로 결제 정보를 조회
-iamport.payment.getByMerchant({
-  merchant_uid: 'your merchant_uid'  
-})
-
-// 상태별 결제 정보 조회
-iamport.payment.getByStatus({
-  payment_status: 'your payment_status'  
+var express = require('express');
+var app = express();
+var server = app.listen(3000, function(){
+    console.log("Express server has started on port 3000")
 })
